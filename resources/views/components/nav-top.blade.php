10e2a89da632eeee67d87fdd6f86464db97f7501
cheiru94 <cheiru94@gmail.com>
2023-11-20 23:58:40 +0900

    a 태그 라우팅

--- resources/views/components/nav-top.blade.php
+++ resources/views/components/nav-top.blade.php
@@ -1,6 +1,6 @@
 <header class="text-gray-600 body-font ">
   
-  <div class=" bg-gray-100  mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+  <div class=" bg-gray-100  mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
     {{-- 1.1 로고--}}
     <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <img class="w-20 h-20  " src="/imgs/logo/hansung-b.png" alt="안뜸">
@@ -11,10 +11,10 @@
     </a>
     {{-- nav bar --}}
     <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-      <a class="mr-10 hover:text-gray-900 text-2xl">한성건축</a>
-      <a class="mr-10 hover:text-gray-900 text-2xl">대표 인사말</a>
-      <a href="{{route('contacts.index')}}" class="mr-10 hover:text-gray-900 text-2xl">커뮤니티</a>
-      <a class="mr-10 hover:text-gray-900 text-2xl">1:1 상담</a>
+      <a href="#" class="mr-10 hover:text-gray-900 text-2xl">한성건축</a>
+      <a href="greeting" class="mr-10 hover:text-gray-900 text-2xl">대표 인사말</a>
+      <a href="#" class="mr-10 hover:text-gray-900 text-2xl">커뮤니티</a>
+      <a href="{{route('contacts.index')}}" class="mr-10 hover:text-gray-900 text-2xl">1:1 상담</a>
     </nav>
     {{-- 로그인 등록 ~ --}}
     @if (Route::has('login'))
