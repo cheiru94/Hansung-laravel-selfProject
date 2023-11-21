be425c2ac5c740e46b7c1ccf461adde622612777
cheiru94 <cheiru94@gmail.com>
2023-11-21 10:14:16 +0900

    type 부분 주석 변경

--- database/migrations/2023_11_19_121039_create_contact_forms_table.php
+++ database/migrations/2023_11_19_121039_create_contact_forms_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title',50); // 1. 문의 사항 
             $table->string('name',20); // 2. 이름 
             $table->string('email', 255); // 3. 이메일
-            $table->string('type',20); // 4. 이메일
+            $table->string('type',20); // 4. 건물 종류
             $table->string('region',20); // 5. 지역
             $table->string('contact', 200); // 6. 문의 내용
             $table->timestamps();
