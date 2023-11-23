654944487c0ba88f47ee5c612217475bd91e00df
cheiru94 <cheiru94@gmail.com>
2023-11-23 16:10:28 +0900

    원래대로

--- database/migrations/2023_11_19_121039_create_contact_forms_table.php
+++ database/migrations/2023_11_19_121039_create_contact_forms_table.php
@@ -6,28 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('contact_forms', function (Blueprint $table) {
-            $table->id();
-            $table->string('title',50); // 1. 문의 사항 
-            $table->string('name',20); // 2. 이름 
-            $table->string('email', 255); // 3. 이메일
-            $table->string('type',20); // 4. 건물 종류
-            $table->string('region',20); // 5. 지역
-            $table->string('contact', 200); // 6. 문의 내용
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('contact_forms', function (Blueprint $table) {
+      $table->id();
+      $table->string('title', 50); // 1. 문의 사항 
+      $table->string('name', 20); // 2. 이름 
+      $table->string('email', 255); // 3. 이메일
+      $table->string('type', 20); // 4. 건물 종류
+      $table->string('region', 20); // 5. 지역
+      $table->string('contact', 200); // 6. 문의 내용
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('contact_forms');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('contact_forms');
+  }
 };
