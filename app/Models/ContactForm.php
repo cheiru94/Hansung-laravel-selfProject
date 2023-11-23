60e9d9673565e12335fe5be7d8299b79479b3133
cheiru94 <cheiru94@gmail.com>
2023-11-23 16:11:43 +0900

    원래대로

--- app/Models/ContactForm.php
+++ app/Models/ContactForm.php
@@ -7,31 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactForm extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-      'title',
-      'name',
-      'email',
-      'type',
-      'region',
-      'contact',
-    ];
+  protected $fillable = [
+    'title',
+    'name',
+    'email',
+    'type',
+    'region',
+    'contact',
+  ];
 
-    public function scopeSearch($query, $search)  // 첫 번째 인자 $query는 쿼리 빌더의 인스턴스, 두 번째 인자 $search는 사용자가 입력한 검색 키워드
-    {
-      // 🟢 검색어가 입력 되어있으면 실행 ( 초기 화면에는 실행되지 않음 )
-      if($search[1] !== null){ 
-   
-        // 하나 이상의 연속된 공백 문자를 찾는 정규 표현식 
-        $search_split2 = preg_split('/[\s]+/', $search[1]); // 한국어 공백 따개기
+  public function scopeSearch($query, $search)  // 첫 번째 인자 $query는 쿼리 빌더의 인스턴스, 두 번째 인자 $search는 사용자가 입력한 검색 키워드
+  {
+    // 🟢 검색어가 입력 되어있으면 실행 ( 초기 화면에는 실행되지 않음 )
+    if ($search[1] !== null) {
 
-        //🟢 따개진 검색어 개수 많큼 반복 하면서 단어 확인
-        foreach( $search_split2 as $value ){
-          $query->where($search[0], 'like', '%' .$value. '%'); 
-        } 
+      // 하나 이상의 연속된 공백 문자를 찾는 정규 표현식 
+      $search_split2 = preg_split('/[\s]+/', $search[1]); // 한국어 공백 따개기
+
+      //🟢 따개진 검색어 개수 많큼 반복 하면서 단어 확인
+      foreach ($search_split2 as $value) {
+        $query->where($search[0], 'like', '%' . $value . '%');
       }
+    }
 
-      return $query; // 생성된 쿼리 반환
-    } 
+    return $query; // 생성된 쿼리 반환
+  }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+
+    
+      </head>
     
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -38,3 +41,4 @@
         </div>
     </body>
 </html>
+
