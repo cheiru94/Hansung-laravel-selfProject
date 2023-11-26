3ff26fbd404af2131e4ca5fb017b8ad339d31103
cheiru94 <cheiru94@gmail.com>
2023-11-26 18:24:55 +0900

    comment 내용 작성

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  // 1. user와 연동 
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  //2. post와 연동
+  public function posts()
+  {
+    return $this->belongsTo(Post::class);
+  }
 }
