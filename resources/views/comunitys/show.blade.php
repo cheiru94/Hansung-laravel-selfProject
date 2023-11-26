12c5501678f51d550e7b41ab43ab16be54c6826c
cheiru94 <cheiru94@gmail.com>
2023-11-26 20:36:41 +0900

    comunity 상세화면

---
+++ resources/views/comunitys/show.blade.php
@@ -0,0 +1,39 @@
+<x-hansung_app.blade>
+  <div class="Contact w-[1440px] h-[1024px] p-2.5 bg-white flex-col justify-start items-center gap-2.5 inline-flex">
+    <div class="TitleDiv w-[1360px] h-[75px] p-2.5 bg-white flex-col justify-start items-start gap-2.5 flex">
+      <div class="Title w-[1080px] grow shrink basis-0 text-black text-[51px] font-normal font-['Inter']">제목</div>
+    </div>
+    <div class="Navbar w-[1366px] h-[68px] px-[35px] py-2.5 bg-slate-50 border-t-2 border-b-2 border-neutral-200 justify-start items-center gap-[29px] inline-flex">
+      <div class="Author w-16 text-black text-[21px] font-bold font-['Inter']">작성자</div>
+      <div class="Authorcontent w-16 text-black text-[21px] font-normal font-['Inter']">관리자</div>
+      <div class="Verticalline w-[27px] h-[0px] origin-top-left rotate-90 border-2 border-zinc-400"></div>
+      <div class="Date w-[89px] text-black text-[21px] font-bold font-['Inter']">작성일자</div>
+      <div class="DateContent w-[235px] text-black text-[21px] font-normal font-['Inter']">1994.10.01 09:10:24</div>
+    </div>
+    <div class="Description w-[1363px] h-[715px] p-2.5 bg-teal-300"></div>
+  </div>
+</x-hansung_app.blade>
+{{-- <section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="-my-8 divide-y-2 divide-gray-100">
+
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-gray-700">CATEGORY</span>
+          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
+          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+          <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+     
+    </div>
+  </div>
+</section> --}}
