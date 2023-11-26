d3a6b994ba58694b709b5558cd7cd4835dd77cc9
cheiru94 <cheiru94@gmail.com>
2023-11-26 20:34:57 +0900

    한성 컴포넌트

---
+++ resources/views/layouts/hansung_app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+  {{-- 🟢 1. top bar --}}
+  <x-nav-top></x-nav-top>
+
+  {{$slot}}
+
+
+
+  {{-- 🟢 3. 푸터 --}}
+  <x-nav-footer></x-nav-footer>
+
+</body>
+</html>
+ 
+ 
