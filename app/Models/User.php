1470d34a9332bf2cf87776c2b71930ba51a61d28
cheiru94 <cheiru94@gmail.com>
2023-12-01 23:33:28 +0900

    관계 재정의

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,36 +10,52 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+    'password' => 'hashed',
+  ];
+
+  /* 🟢 관계 정의 🟢 */
+  public function contactForms()
+  {
+    return $this->hasOne(ContactForm::class);
+  }
+
+  public function posts()
+  {
+    return $this->hasOne(Post::class);
+  }
+
+  public function commments()
+  {
+    return $this->hasOne(Comment::class);
+  }
 }
