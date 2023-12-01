4ad137a58acc22fad52b026a2e02052dc3dca4ee
cheiru94 <cheiru94@gmail.com>
2023-12-02 00:48:26 +0900

    user_id 추가

--- database/factories/ContactFormFactory.php
+++ database/factories/ContactFormFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,22 +10,23 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ContactFormFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-      $types = ['house', 'villa', 'apartment', 'shop', 'supervision'];
-      $regions = ['남구', '중구', '북구', '북구', '동구','울주군','그 외 지역'];
-        return [
-          'title' => $this->faker->realText(50), 
-          'name' => $this->faker->name(20), 
-          'email' => $this->faker->email(), 
-          'type' => $this->faker->randomElement($types),
-          'region' => $this->faker->randomElement($regions), 
-          'contact' => $this->faker->realText(200),
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    $types = ['house', 'villa', 'apartment', 'shop', 'supervision'];
+    $regions = ['남구', '중구', '북구', '북구', '동구', '울주군', '그 외 지역'];
+    return [
+      'title' => $this->faker->realText(50),
+      'name' => $this->faker->name(20),
+      'email' => $this->faker->email(),
+      'type' => $this->faker->randomElement($types),
+      'region' => $this->faker->randomElement($regions),
+      'contact' => $this->faker->realText(200),
+      'user_id' => User::all()->random()->id,
+    ];
+  }
 }
