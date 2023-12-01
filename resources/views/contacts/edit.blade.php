3fc8f8b5432bfc456d9de91f0e59facd9a6e342b
cheiru94 <cheiru94@gmail.com>
2023-12-01 23:38:28 +0900

    layout -> hansung으로 변경

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -1,9 +1,4 @@
-<x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          편집하기
-      </h2>
-  </x-slot>
+<x-hansung-layout >
 
   <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -97,7 +92,7 @@
           </div>
       </div>
   </div>
-</x-app-layout>
+</x-hansung-layout>
 <script>
   $('#contact').summernote({
     placeholder: '안녕하세요, 한성 건축사사무소',
--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -1,9 +1,4 @@
-<x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          상세화면
-      </h2>
-  </x-slot>
+<x-hansung-layout>
 
    
   <div class="py-12">
@@ -130,5 +125,5 @@
     }
   </script>
 
-</x-app-layout>
+</x-hansung-layout>
  
